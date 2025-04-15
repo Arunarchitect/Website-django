031d4e3f107f2411015faa5de58f713778417283
Arun <intformbase@gmail.com>
2025-04-15 14:06:32 +0530

    added sum in admin panel

--- expense/admin.py
+++ expense/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
+from django.db.models import Sum
 from .models import Expense, Item, Category, Brand, Shop
 
-admin.site.register(Item)
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Shop)
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
@@ -13,6 +10,16 @@ class ExpenseAdmin(admin.ModelAdmin):
         'date_of_purchase', 'quantity', 'unit', 'price', 'rate', 'remarks'
     )
     readonly_fields = ('rate',)
-    # search_fields = (...)  # Temporarily comment out
-    # list_filter = (...)    # Temporarily comment out
     ordering = ('-date_of_purchase',)
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context)
+
+        try:
+            qs = response.context_data['cl'].queryset
+            total_price = qs.aggregate(total=Sum('price'))['total'] or 0
+            response.context_data['title'] += f" (Total Price: ₹{total_price:.2f})"
+        except (AttributeError, KeyError):
+            pass
+
+        return response

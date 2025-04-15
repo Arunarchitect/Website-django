691f7bf746c324c00998c18ee011f4885ced2777
Arun <intformbase@gmail.com>
2025-04-15 14:10:09 +0530

    corrected expense admin panel

--- expense/admin.py
+++ expense/admin.py
@@ -3,7 +3,6 @@ from django.db.models import Sum
 from .models import Expense, Item, Category, Brand, Shop
 
 
-@admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'who_spent', 'item', 'category', 'brand', 'shop',
@@ -14,12 +13,18 @@ class ExpenseAdmin(admin.ModelAdmin):
 
     def changelist_view(self, request, extra_context=None):
         response = super().changelist_view(request, extra_context)
-
         try:
             qs = response.context_data['cl'].queryset
             total_price = qs.aggregate(total=Sum('price'))['total'] or 0
             response.context_data['title'] += f" (Total Price: ₹{total_price:.2f})"
         except (AttributeError, KeyError):
             pass
-
         return response
+
+
+# Register all models properly
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Shop)

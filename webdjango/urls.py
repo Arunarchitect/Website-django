d0c57166f808ea367e431e30f8409099f5ab95f0
Arun <intformbase@gmail.com>
2025-04-25 13:06:58 +0530

    corrected urls with correct http import

--- webdjango/urls.py
+++ webdjango/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from projects.views import ProjectViewSet, WorkTypeViewSet, WorkLogViewSet, DeliverableViewSet
+from django.http import HttpResponse
+
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet)

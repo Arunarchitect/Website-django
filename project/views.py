b96b50108ae703b8ab634617cdfe876a2b683823
Arun <intformbase@gmail.com>
2025-04-29 08:20:38 +0530

    added csv download

--- project/views.py
+++ project/views.py
@@ -1,7 +1,8 @@
+import csv
+from django.http import HttpResponse
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets
-from django.db.models import Sum, F, ExpressionWrapper, DurationField
 
 from .models import Project, WorkLog, Deliverable
 from .serializers import ProjectSerializer, WorkLogSerializer, DeliverableSerializer
@@ -11,49 +12,53 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
-    @action(detail=True, methods=['get'])
-    def summary(self, request, pk=None):
-        project = self.get_object()
-
-        logs = WorkLog.objects.filter(deliverable__project=project, end_time__isnull=False)
-
-        total_duration = logs.aggregate(
-            duration=Sum(
-                ExpressionWrapper(F('end_time') - F('start_time'), output_field=DurationField())
-            )
-        )['duration']
-
-        work_summary = logs.values('deliverable__name').annotate(
-            duration=Sum(
-                ExpressionWrapper(F('end_time') - F('start_time'), output_field=DurationField())
-            )
-        )
-
-        deliverables = project.deliverables.values('name', 'stage', 'status', 'remarks')
-
-        return Response({
-            'project': project.name,
-            'current_stage': project.current_stage,
-            'total_duration_seconds': total_duration.total_seconds() if total_duration else 0,
-            'deliverables_summary': [
-                {
-                    'name': item['deliverable__name'],
-                    'duration_seconds': item['duration'].total_seconds() if item['duration'] else 0
-                }
-                for item in work_summary
-            ],
-            'deliverables': list(deliverables)
-        })
-
 
 class WorkLogViewSet(viewsets.ModelViewSet):
     queryset = WorkLog.objects.all()
     serializer_class = WorkLogSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(employee=self.request.user)
+    @action(detail=False, methods=['get'])
+    def download_csv(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="worklogs.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Employee', 'Deliverable', 'Project', 'Start Time', 'End Time', 'Duration (seconds)'])
+
+        for worklog in self.get_queryset().select_related('employee', 'deliverable__project'):
+            writer.writerow([
+                worklog.employee.email,
+                worklog.deliverable.name if worklog.deliverable else '',
+                worklog.deliverable.project.name if worklog.deliverable and worklog.deliverable.project else '',
+                worklog.start_time,
+                worklog.end_time,
+                worklog.duration if worklog.duration else '',
+            ])
+
+        return response
 
 
 class DeliverableViewSet(viewsets.ModelViewSet):
     queryset = Deliverable.objects.all()
     serializer_class = DeliverableSerializer
+
+    @action(detail=False, methods=['get'])
+    def download_csv(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="deliverables.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Project', 'Deliverable Name', 'Stage', 'Status', 'Remarks', 'Start Date', 'End Date'])
+
+        for deliverable in self.get_queryset().select_related('project'):
+            writer.writerow([
+                deliverable.project.name if deliverable.project else '',
+                deliverable.name,
+                deliverable.get_stage_display(),
+                deliverable.get_status_display(),
+                deliverable.remarks,
+                deliverable.start_date,
+                deliverable.end_date,
+            ])
+
+        return response

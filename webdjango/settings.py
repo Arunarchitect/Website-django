887c320af2556862811dbb047c11d93543c3374b
anumol998 <anumolregi998@gmail.com>
2025-09-07 11:39:05 +0530

    added refresh functionality correction

--- webdjango/settings.py
+++ webdjango/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     # packages
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',  # 👈 Add this
     'corsheaders',
     'djoser',
     'social_django',
@@ -88,18 +89,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'webdjango.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',  # The name of your PostgreSQL database
-        'USER': 'myuser',  # The PostgreSQL user you created
-        'PASSWORD': 'mypassword',  # The PostgreSQL user password
-        'HOST': '173.212.238.69',  # The IP address of your VPS
-        'PORT': '5432',  # Default PostgreSQL port
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 # Password validation
@@ -259,10 +259,10 @@ AUTH_COOKIE_SAMESITE = 'None'
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),     # ⏳ You can increase this if needed
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),     # ⏳ You can increase this if needed
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,                     # Optional: new refresh token each time
-    'BLACKLIST_AFTER_ROTATION': True,                  # Optional: invalidate old ones after rotation
+    'ROTATE_REFRESH_TOKENS': False,                     # Optional: new refresh token each time
+    'BLACKLIST_AFTER_ROTATION': False,                  # Optional: invalidate old ones after rotation
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
 }

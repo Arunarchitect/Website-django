f26adbde88a05b88823d14d9035281cbe1b8fb71
Arun Ravikumar <intformbase@gmail.com>
2025-09-17 13:32:35 +0530

    Update models.py to delete and override media files

--- viewer/models.py
+++ viewer/models.py
@@ -1,13 +1,14 @@
-# viewer/models.py
-
 import uuid
-import os
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
 from project.models import Project, Organisation
 from .validators import validate_360_image
 
 User = get_user_model()
+
 
 class Viewer(models.Model):
     user = models.ForeignKey(
@@ -28,7 +29,7 @@ class Viewer(models.Model):
 
     view_name = models.CharField(max_length=255)
     view_date = models.DateField()
-    
+
     image_360 = models.ImageField(
         upload_to='viewer/360_images/',
         validators=[validate_360_image],
@@ -41,6 +42,27 @@ class Viewer(models.Model):
 
     def __str__(self):
         return f"{self.project.name} - {self.view_name} ({self.view_date})"
+
+    def save(self, *args, **kwargs):
+        """Delete old file when updating with a new one."""
+        if self.pk:
+            try:
+                old_file = Viewer.objects.get(pk=self.pk).image_360
+            except Viewer.DoesNotExist:
+                old_file = None
+
+            new_file = self.image_360
+            if old_file and old_file != new_file:
+                old_file.delete(save=False)
+
+        super().save(*args, **kwargs)
+
+
+# --- CLEANUP HANDLER ON DELETE (covers both instance.delete() & queryset.delete()) ---
+@receiver(post_delete, sender=Viewer)
+def delete_file_on_instance_delete(sender, instance, **kwargs):
+    if instance.image_360:
+        instance.image_360.delete(save=False)
 
 
 class ProjectAccessKey(models.Model):
